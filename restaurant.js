61feeb6e6943436561a8ca8a6b77b928f515fc85
Impulsum Esien <gideonesienss@gmail.com>
2026-09-21 21:26:46 +0100

    Added welcome popup menu

--- restaurant.js
+++ restaurant.js
@@ -5,3 +5,14 @@ const nav=document.querySelector("nav");
 menu.addEventListener("click", (event)=>{
   nav.classList.toggle("show");
 });
+
+const modal=document.querySelector(".modal");
+const closeModal=document.querySelector("#closeModal");
+
+window.addEventListener("load", ()=>{
+  modal.classList.add("display");
+});
+
+closeModal.addEventListener("click", ()=>{
+  modal.classList.remove("display");
+})
